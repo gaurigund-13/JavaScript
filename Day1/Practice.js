9083aefb17f217ce068d39dcdd35267d6210bb23
gaurigund-13 <gaurigund14@gmail.com>
2024-05-20 19:46:42 +0530

    second commit

---
+++ Day1/Practice.js
@@ -0,0 +1,24 @@
+// Que) Create a const object called "product" to store information shown in figure
+
+const product = {
+    pName : "Ball Pen",
+    ratting : 4 ,
+    offer : 5,
+    price : 270,
+
+};
+
+console.log(product);
+
+// Que) Create a const object called "profile" to store information shown in it.
+
+const profile = {
+    userName : "@shradhakhapra",
+    post : 195,
+    followers : 569000,
+    following : 4,
+    isFollow : true,
+    bio : "Apna College | Ex-Microsoft, DRDO"
+};
+
+console.log(profile)
--- Day1/first.js
+++ Day1/first.js
@@ -1,1 +1,29 @@
 console.log('Namaste JavaScript');
+console.log('Gauri Gund');
+
+// var :- re-declared and updated
+var Age = 20;
+
+var Age = 34;
+
+// let :- cannot be re-declared but update.
+let age = 22;
+
+age = 45;
+
+console.log(age)
+
+// const :- cannot be re-declared and updated
+const pi= 3.14
+console.log(pi)
+
+// object
+const student ={
+    fullName : "Gauri",
+    age : 23,
+    cgpa : 9.42,
+    isPass : true
+};
+
+console.log(student['fullName']);
+
